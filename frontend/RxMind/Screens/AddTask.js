f9ed472f9dd253878bb7b276876e935abacf3ecd
Advit Singh <advitsingh@gmail.com>
2025-06-30 20:21:38 -0500

    reorganize structure

--- frontend/RxMind/Screens/AddTask.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function AddTask() {
-  return (
-    <View style={{ padding: 30 }}>
-      <Text style={{ fontSize: 18 }}>This is the AddTask screen.</Text>
-    </View>
-  );
-}
--- frontend/RxMind/Screens/Checklist.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function Checklist() {
-  return (
-    <View style={{ padding: 30 }}>
-      <Text style={{ fontSize: 18 }}>This is the Checklist screen.</Text>
-    </View>
-  );
-}

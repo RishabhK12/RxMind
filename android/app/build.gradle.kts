7433c1472482d0fb30a886be90dbb5acbe1d726c
RishabhK12 <rishabhkapoor@live.com>
2025-12-24 16:53:59 -0700

    Bump version for app bundle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.rxmind.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
     }
 
     // --- Load keystore properties ---

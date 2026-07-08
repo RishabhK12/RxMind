661c22b17e8270c5460e49b0101575f7d9606810
Advit Singh <advitsingh@gmail.com>
2026-07-08 14:52:43 +0530

    feat(p1-t1.9): migrate application ID to com.rxmind.app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     ndkVersion = "27.0.12077973"
-    namespace = "com.example.rxmind_app"
+    namespace = "com.rxmind.app"
     compileSdk = flutter.compileSdkVersion
 
     compileOptions {
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.rxmind_app"
+        applicationId = "com.rxmind.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
